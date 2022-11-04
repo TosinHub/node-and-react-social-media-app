0088c2125d7840c5d280068da72ce7fb10679599
TosinHub <tosinolugbenga@gmail.com>
2022-11-04 06:10:54 +0000

    installations

---
+++ backend/schemas/save.js
@@ -0,0 +1,19 @@
+
+export default {
+    name: 'save',
+    title: 'Save',
+    type: 'document',
+    fields: [
+        {
+            name: 'postedBy',
+            title: 'postedBy',
+            type: 'postedBy'
+        },
+
+        {
+            name: 'userId',
+            title: 'UserId',
+            type: 'string'
+        }
+    ]
+}
